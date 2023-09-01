e0c1464006b7c6ca08380bf2e998b5f9a610aafa
Nauman Bashir <nauman.bashir@hotmail.com>
2023-09-01 21:23:25 +0500

    Replaced ResponseCode Constants with HttpResponse enum class. Added API response helper methods.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\enums\HttpResponse;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function successResponse($data = [], $code = HttpResponse::OK, $message = ''): JsonResponse
+    {
+        return response()->json([
+            'status' => 'success',
+            'code' => $code,
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+
+    protected function failResponse(string $message, $code = HttpResponse::BAD_REQUEST): JsonResponse
+    {
+        return response()->json([
+            'status' => 'failure',
+            'code' => $code,
+            'message' => $message,
+//            'data' => $data
+        ], $code);
+    }
 }
